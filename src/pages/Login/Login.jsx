4be8aad13b1a71d58bfbecb8dcbe0d243441ce4e
unknown <luizhmatoso1340@gmail.com>
2025-04-30 14:24:04 -0300

    feat: add login form and submit button

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { assets } from "../../assets/assets";
 import { Link } from "react-router-dom";
 
+import "./Login.css";
+
 const Login = () => {
   return (
     <div
@@ -24,7 +26,7 @@ const Login = () => {
           to="/"
           style={{
             display: "flex",
-            gap: "5px",
+            gap: 5,
             alignItems: "center",
             fontWeight: "bold",
             fontSize: "24px",
@@ -34,6 +36,45 @@ const Login = () => {
           <img src={assets.logo_white} alt="" width={32} height={32} />
           <span className="fw-bold fs-4 text-light">GateWayz</span>
         </Link>
+      </div>
+
+      <div className="card p-4" style={{ maxWidth: "400px", width: "100%" }}>
+        <h2 className="text-center mb-4">Login</h2>
+        <form>
+          <div className="mb-3">
+            <label htmlFor="email" className="form-label">
+              Email
+            </label>
+            <input
+              type="text"
+              id="email"
+              className="form-control"
+              placeholder="Enter your email"
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="password" className="form-label">
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              className="form-control"
+              placeholder="Enter your password"
+              required
+            />
+          </div>
+          <div className="d-flex justify-content-between mb-3">
+            <Link to="/reset-password" className="text-decoration-none">
+              Forgot password?
+            </Link>
+          </div>
+
+          <button type="submit" className="btn btn-grad text-white w-100">
+            Login
+          </button>
+        </form>
       </div>
     </div>
   );
